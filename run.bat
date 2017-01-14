09131e4cf0091440bf8b534cb32f37e1bb206825
zinc1oxide@gmail.com <zinc1oxide@gmail.com>
2017-01-14 11:36:36 -0500

    Added token to pass command-line parameters

--- run.bat
+++ run.bat
@@ -18,5 +18,4 @@ set JAVA="%JAVA_HOME%\bin\java"
 set CLASSPATH=.\bin;%EMA_HOME%\Libs\ema.jar;%EMA_HOME%\Libs\SLF4J\slf4j-1.7.12\slf4j-api-1.7.12.jar;%EMA_HOME%\Libs\SLF4J\slf4j-1.7.12\slf4j-jdk14-1.7.12.jar;%EMA_HOME%\Libs\apache\commons-configuration-1.10.jar;%EMA_HOME%\Libs\apache\commons-logging-1.2.jar;%EMA_HOME%\Libs\apache\commons-lang-2.6.jar;%ETA_HOME%\Libs\upa.jar;%ETA_HOME%\Libs\upaValueAdd.jar;%JAVA_FX%\jfxswt.jar
 
 echo Running Speed Guide...
-rem %JAVA% -cp %CLASSPATH% com.thomsonreuters.ema.example.gui.SpeedGuide.SpeedGuide
 %JAVA% -cp %CLASSPATH% com.thomsonreuters.ema.example.gui.SpeedGuide.SpeedGuide %*
